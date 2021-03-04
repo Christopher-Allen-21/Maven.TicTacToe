cbe24ecd84c573ca0c3796ab644aa6e748901bde
Chris Allen <Chris@zipcodes-MacBook-Pro-350.local>
2021-03-04 15:50:24 -0500

    Simplified

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -12,37 +12,22 @@ public class Board {
 
     public Boolean isInFavorOfX() {
         boolean xWon = false;
-            if(checkColumn('X')){
-                xWon = true;
-            }
-            else if(checkRow('X')){
-                xWon = true;
-            }
-            else if(checkDiag('X')){
-                xWon = true;
+            if(checkWin('X')){
+                return true;
             }
             else{
-                xWon = false;
+                return false;
             }
-        return xWon;
     }
 
     public Boolean isInFavorOfO() {
-        boolean oWon = false;
-            if(checkColumn('O')){
-                oWon = true;
-            }
-            else if(checkRow('O')){
-                oWon = true;
-            }
-            else if(checkDiag('O')){
-                oWon = true;
-            }
-            else{
-                oWon = false;
-            }
-        return oWon;
-
+        boolean xWon = false;
+        if(checkWin('O')){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     public Boolean isTie() {
@@ -66,7 +51,7 @@ public class Board {
         }
     }
 
-    public boolean checkColumn(char playerXY) {
+    public boolean checkWin(char playerXY) {
         int count = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -81,46 +66,4 @@ public class Board {
             return false;
         }
     }
-
-
-    public boolean checkRow(char playerXY){
-        int count = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (matrix[i][j] == playerXY) {
-                    count++;
-                }
-            }
-        }
-        if (count == 3) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean checkDiag(char playerXY){
-        int countLeftDiag = 0;
-        int countRightDiag = 0;
-
-        for(int i=0;i<matrix.length;i++){
-            if(matrix[i][i].equals(playerXY)){
-                countLeftDiag++;
-            }
-        }
-        for(int i=0;i<matrix.length;i++){
-            if(matrix[i][matrix.length-i-1].equals(playerXY)){
-                countRightDiag++;
-            }
-        }
-
-
-        if(countLeftDiag==3 || countRightDiag==3){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
 }
